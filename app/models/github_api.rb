3be5d4b88e79230785489251ef75359529675658
Doug Wright <mrdougwright@gmail.com>
2017-02-15 12:25:40 -0800

    add oauth for github api

--- app/models/github_api.rb
+++ app/models/github_api.rb
@@ -2,7 +2,7 @@ class GithubAPI
   attr_reader :github, :user_name
 
   def initialize(user_name)
-    @github = Github.new
+    @github = Github.new client_id: ENV['GITHUB_CLIENT_ID'], client_secret: ENV['GITHUB_CLIENT_SECRET']
     @user_name = user_name
   end
 
