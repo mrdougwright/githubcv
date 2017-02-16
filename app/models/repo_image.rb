367d0889896a6a26c810d813b7cbf12dea19f2f3
Doug Wright <mrdougwright@gmail.com>
2017-02-16 11:36:07 -0800

    fix obj overwriting issue

--- app/models/repo_image.rb
+++ app/models/repo_image.rb
@@ -11,24 +11,24 @@ class RepoImage < ApplicationRecord
   end
 
   def self.create_obj(repo)
-    obj = self.create(url: repo.homepage, desc: repo.description, name: repo.name, repo_name: repo.full_name)
-    self.create_webshot(obj, repo)
+    new_obj = self.create(url: repo.homepage, desc: repo.description, name: repo.name, repo_name: repo.full_name)
+    self.create_webshot(new_obj, repo)
   end
 
   def self.update_obj(repo)
-    obj = RepoImage.find_by_repo_name(repo.full_name)
-    obj.update_attributes(url: repo.homepage, desc: repo.description, name: repo.name, repo_name: repo.full_name)
-    self.create_webshot(obj, repo)
+    old_obj = RepoImage.find_by_repo_name(repo.full_name)
+    old_obj.update_attributes(url: repo.homepage, desc: repo.description, name: repo.name, repo_name: repo.full_name)
+    self.create_webshot(old_obj, repo)
   end
 
   def self.create_webshot(repo_obj, repo)
     filename = "#{repo.full_name.gsub('/','-')}.png"
     begin
       ws = Webshot::Screenshot.instance # tried to add sidekiq to this
-      file = ws.capture(repo.homepage, filename, timeout: 10, width: 600, height: 400)
-      obj = S3_BUCKET.object(filename)
-      if obj.upload_file(file.path)
-        repo_obj.update_attribute(:image_url, obj.public_url)
+      file = ws.capture(repo.homepage, filename, timeout: 5, width: 500, height: 200)
+      s3_obj = S3_BUCKET.object(filename)
+      if s3_obj.upload_file(file.path)
+        repo_obj.update_attribute(:image_url, s3_obj.public_url)
       end
     rescue Exception => e
       puts "Error in create_webshot: #{e}"
