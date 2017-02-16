e0922de95ae203c36f10da16012ffa06399153d5
Doug Wright <mrdougwright@gmail.com>
2017-02-16 11:25:17 -0800

    update option

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -2,6 +2,7 @@ class ReposController < ApplicationController
 
   def show
     repos_hash = GithubAPI.new(params[:user_name]).repos_hash
+    @repos.each {|r| RepoImage.update_obj(r)} if params[:update] == true
     @repos = repos_hash.map do |repo|
       RepoImage.find_or_create(repo)
     end
--- app/models/repo_image.rb
+++ app/models/repo_image.rb
@@ -2,14 +2,27 @@ class RepoImage < ApplicationRecord
 
   def self.find_or_create(repo)
     obj = self.find_by_repo_name(repo.full_name)
-    return obj if obj && !obj.image_url.nil?
-    self.create_webshot(repo)
+    if obj
+      return obj if obj.image_url.present?
+      self.create_webshot(obj, repo)
+    else
+      self.create_obj(repo)
+    end
   end
 
-  def self.create_webshot(repo)
-    filename = "#{repo.full_name.gsub('/','-')}.png"
-    repo_obj = self.create(url: repo.homepage, desc: repo.description, name: repo.name, repo_name: repo.full_name)
+  def self.create_obj(repo)
+    obj = self.create(url: repo.homepage, desc: repo.description, name: repo.name, repo_name: repo.full_name)
+    self.create_webshot(obj, repo)
+  end
 
+  def self.update_obj(repo)
+    obj = RepoImage.find_by_repo_name(repo.full_name)
+    obj.update_attributes(url: repo.homepage, desc: repo.description, name: repo.name, repo_name: repo.full_name)
+    self.create_webshot(obj, repo)
+  end
+
+  def self.create_webshot(repo_obj, repo)
+    filename = "#{repo.full_name.gsub('/','-')}.png"
     begin
       ws = Webshot::Screenshot.instance # tried to add sidekiq to this
       file = ws.capture(repo.homepage, filename, timeout: 10, width: 600, height: 400)
@@ -17,7 +30,6 @@ class RepoImage < ApplicationRecord
       if obj.upload_file(file.path)
         repo_obj.update_attribute(:image_url, obj.public_url)
       end
-      AWS.config.http_handler.pool.empty! # clear bad memory in aws gem
     rescue Exception => e
       puts "Error in create_webshot: #{e}"
     end
