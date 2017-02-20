18e68b65d4acf84c1371e15f4accf80b56310444
Doug Wright <mrdougwright@gmail.com>
2017-02-20 13:18:36 -0800

    fix singular api call

--- app/controllers/api/v1/api_repos_controller.rb
+++ app/controllers/api/v1/api_repos_controller.rb
@@ -10,7 +10,8 @@ class Api::V1::ApiReposController < ApplicationController
 
   def repo_image
     repos_hash = GithubAPI.new(params[:user_name]).repos_hash
-    @repo = repos_hash.select{|r| r.name == params[:repo_name]}.first
+    repo = repos_hash.select{|r| r.name == params[:repo_name]}.first
+    @repo = RepoImage.find_or_create(repo)
     render json: @repo
   end
 end
